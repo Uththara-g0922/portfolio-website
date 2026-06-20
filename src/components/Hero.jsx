a11b8a8ae5006f9c1ab9a1c2cff26c072b7cc7cf
Uththara Nethmini <uththaranethmini2005@outlook.com>
2026-06-20 19:39:54 +0530

    added darkmode togglr

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import instagram from '../assets/instagram.png'
+import tiktok from '../assets/tiktok.png'
+import github from '../assets/github.png'
+import youtube from '../assets/youtube.png'
+
+const Hero = () => {
+  const socialIcons = [
+    { icon: instagram, alt: 'Instagram'},
+    { icon: tiktok, alt: 'tiktok'},
+    { icon: github, alt: 'github'},
+    { icon: youtube, alt: 'youtube'},
+  ];
+
+  
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
